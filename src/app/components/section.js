fcbc01a78750be2cd25721119e1da5a08c572da6
unknown <devjosm@gmail.com>
2024-12-08 15:07:09 +0100

    Minor fix

--- src/app/components/section.js
+++ src/app/components/section.js
@@ -3,16 +3,16 @@ import {FaChevronDown, FaChevronUp} from 'react-icons/fa6'
 export default function Section({icon, text, content, tag, activeSection, setActiveSection}) {
     return (
     <div>
-      <button className="flex w-full justify-between items-center bg-slate-900 md:rounded-t-md p-2 text-slate-200 text-xl font-bold justify-between" onClick={() => setActiveSection(tag)}>
+      <div className="flex w-full justify-between items-center bg-slate-900 md:rounded-t-md p-2 text-slate-200 text-xl font-bold justify-between">
         <p className="flex items-center gap-1.5">
             {icon}
             {text}
         </p>
-        <p>{activeSection === tag ? <FaChevronUp /> : <FaChevronDown />}</p>
-      </button>
-        <div className={`${activeSection === tag ?'flex' : 'hidden'} md:flex gap-3 px-3 py-3 bg-slate-800 md:rounded-b-md`}>
+        <p className="block md:hidden">{activeSection === tag ? <FaChevronUp /> : <FaChevronDown />}</p>
+      </div>
+        <button className={`${activeSection === tag ?'flex' : 'hidden'} md:flex gap-3 px-3 py-3 bg-slate-800 md:rounded-b-md`} onClick={() => setActiveSection(tag)}>
             {content}
-        </div>
+        </button>
     </div>
     );
   }
